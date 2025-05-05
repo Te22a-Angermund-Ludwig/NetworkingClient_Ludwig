abec83729c4b4325f69e85ad499f9931656b9403
Ludwig Angermund <Ludwig.angermund@elev.ga.ntig.se>
2025-05-05 11:14:33 +0200

    påbörjat koden, slut på lektionen

--- src/NetworkingClient.java
+++ src/NetworkingClient.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class NetworkingClient {
+    public static void main(String args[]) {
+
+        System.out.println("Enter your name. Type Bye to exit. ");
+
+        msg = stdIn.readline().trim();
+
+    }
 }
